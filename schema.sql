10025758243cbb988544badfa8d07a7dff6e989b
Beryl Koko <157668572+BerylKoko@users.noreply.github.com>
2026-09-13 05:54:41 -0400

    Add SQL schema placeholder

---
+++ schema.sql
@@ -0,0 +1,10 @@
+-- Job Market & Skills Analysis
+-- TODO: define normalized tables, primary keys, and foreign keys.
+
+-- Suggested tables:
+-- industries
+-- companies
+-- locations
+-- job_postings
+-- skills
+-- job_skills
